2d7a4aab2a0c3b20bb81f014bc52eff9801d1a6c
Luna <advaith.madhukar@gmail.com>
2021-08-28 19:15:34 +0530

    feat: support for zstd compressed rpms

--- src/rpm/compressor.rs
+++ src/rpm/compressor.rs
@@ -1,10 +1,10 @@
-use std::io::Write;
-
 use crate::errors::*;
+use std::io::Write;
 
 pub enum Compressor {
     None(Vec<u8>),
     Gzip(libflate::gzip::Encoder<Vec<u8>>),
+    Zstd(zstd::stream::Encoder<'static, Vec<u8>>),
 }
 
 impl Write for Compressor {
@@ -12,22 +12,28 @@ impl Write for Compressor {
         match self {
             Compressor::None(data) => data.write(content),
             Compressor::Gzip(encoder) => encoder.write(content),
+            Compressor::Zstd(encoder) => encoder.write(content),
         }
     }
     fn flush(&mut self) -> Result<(), std::io::Error> {
         match self {
             Compressor::None(data) => data.flush(),
             Compressor::Gzip(encoder) => encoder.flush(),
+            Compressor::Zstd(encoder) => encoder.flush(),
         }
     }
 }
-
+// 19 is used here as its 19 for fedora
 impl std::str::FromStr for Compressor {
     type Err = RPMError;
     fn from_str(raw: &str) -> Result<Self, Self::Err> {
         match raw {
             "none" => Ok(Compressor::None(Vec::new())),
             "gzip" => Ok(Compressor::Gzip(libflate::gzip::Encoder::new(Vec::new())?)),
+            "zstd" => Ok(Compressor::Zstd(zstd::stream::Encoder::new(
+                Vec::new(),
+                19,
+            )?)),
             _ => Err(RPMError::UnknownCompressorType(raw.to_string())),
         }
     }
@@ -38,6 +44,7 @@ impl Compressor {
         match self {
             Compressor::None(data) => Ok(data),
             Compressor::Gzip(encoder) => Ok(encoder.finish().into_result()?),
+            Compressor::Zstd(encoder) => Ok(encoder.finish().unwrap()),
         }
     }
 
@@ -47,6 +54,10 @@ impl Compressor {
             Compressor::Gzip(_) => Some(CompressionDetails {
                 compression_level: "9",
                 compression_name: "gzip",
+            }),
+            Compressor::Zstd(_) => Some(CompressionDetails {
+                compression_level: "19",
+                compression_name: "zstd",
             }),
         }
     }
