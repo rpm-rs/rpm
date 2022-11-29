8f94f5eafef5fe14f4fb5ad28c0b5e14f3743feb
Max Dymond <cmeister2@gmail.com>
2022-11-29 14:53:10 +0000

    Add a test which tests `with_file_async` if `async-tokio` is enabled.
    Fixes #44

--- src/tests.rs
+++ src/tests.rs
@@ -316,6 +316,42 @@ async fn test_rpm_header_async() -> Result<(), Box<dyn std::error::Error>> {
     test_rpm_header_base(package)
 }
 
+#[cfg(feature = "async-tokio")]
+#[tokio::test]
+async fn test_rpm_builder_async() -> Result<(), Box<dyn std::error::Error>> {
+    use std::str::FromStr;
+
+    let mut buff = std::io::Cursor::new(Vec::<u8>::new());
+
+    let pkg = rpm::RPMBuilder::new("test", "1.0.0", "MIT", "x86_64", "some awesome package")
+        .compression(rpm::Compressor::from_str("gzip")?)
+        .with_file_async(
+            "Cargo.toml",
+            RPMFileOptions::new("/etc/awesome/config.toml").is_config(),
+        )
+        .await?
+        // file mode is inherited from source file
+        .with_file_async("Cargo.toml", RPMFileOptions::new("/usr/bin/awesome"))
+        .await?
+        .with_file_async(
+            "Cargo.toml",
+            // you can set a custom mode and custom user too
+            RPMFileOptions::new("/etc/awesome/second.toml")
+                .mode(0o100744)
+                .user("hugo"),
+        )
+        .await?
+        .pre_install_script("echo preinst")
+        .add_changelog_entry("me", "was awesome, eh?", 123123123)
+        .add_changelog_entry("you", "yeah, it was", 12312312)
+        .requires(Dependency::any("wget"))
+        .build()?;
+
+    pkg.write(&mut buff)?;
+
+    Ok(())
+}
+
 #[test]
 fn test_rpm_header() -> Result<(), Box<dyn std::error::Error>> {
     let rpm_file_path = test_rpm_file_path();
