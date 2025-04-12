462c6710b9d88028d10b189b2b412f800c476eaf
Daniel Alley <dalley@redhat.com>
2025-04-12 11:29:36 -0400

    Clean up compat test logic

--- tests/assets/import_keys.sh
+++ tests/assets/import_keys.sh
@@ -1,13 +1,18 @@
+#!/bin/sh
+
+passphrase="thisisN0Tasecuredpassphrase"
+
 gpg --import ./signing_keys/public_rsa4096.asc
 gpg --import ./signing_keys/secret_rsa4096.asc
 gpg --import ./signing_keys/public_rsa3072_protected.asc
-gpg --import ./signing_keys/secret_rsa3072_protected.asc
+gpg --batch --passphrase="${passphrase}" --pinentry-mode=loopback --import ./signing_keys/secret_rsa3072_protected.asc
 gpg --import ./signing_keys/public_ed25519.asc
 gpg --import ./signing_keys/secret_ed25519.asc
 gpg --import ./signing_keys/public_ecdsa_nistp256.asc
 gpg --import ./signing_keys/secret_ecdsa_nistp256.asc
 
-sudo rpm -v --import ./signing_keys/public_rsa4096.asc
-sudo rpm -v --import ./signing_keys/public_rsa3072_protected.asc
-sudo rpm -v --import ./signing_keys/public_ed25519.asc
-sudo rpm -v --import ./signing_keys/public_ecdsa_nistp256.asc
+rpm -v --import ./signing_keys/public_rsa4096.asc
+rpm -v --import ./signing_keys/public_rsa3072_protected.asc
+# this will fail with older versions of RPM, so for the sake of better UX we ignore failures
+rpm -v --import ./signing_keys/public_ed25519.asc || true
+rpm -v --import ./signing_keys/public_ecdsa_nistp256.asc || true
